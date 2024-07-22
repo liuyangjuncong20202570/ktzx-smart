0dc62e1843dbdb817de1201aaa51be4271fa197b
MengXingTong <67214758+MengXingTong@users.noreply.github.com>
2024-07-22 21:58:12 +0800

    新增考核方案界面跳转

--- src/router/index.js
+++ src/router/index.js
@@ -159,11 +159,15 @@ const routes = [
                 name: 'CourseTarget',
                 component: () => import('../components/evaluation/CourseTarget.vue')  
             },
-
             {
                 path: 'exam/coursequelib', // 达成性评价模型-课程目标
                 name: 'CourseTarget',
                 component: () => import('../components/evaluation/CourseTarget.vue')
+            },
+            {
+                path: 'evasys/accessible/AssessmentTable', // 达成性评价模型-评价表
+                name: 'AssessmentTable',
+                component: () => import('../components/evaluation/AssessmentTable.vue')
             },
         ]
     },
