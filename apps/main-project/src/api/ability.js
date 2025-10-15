ba7578632cbdf31ccba85ec17b084f24c1ca0b35
liuyangjuncong <liuyangjuncong@meituan.com>
2025-10-15 20:56:21 +0800

    feat:add config

--- apps/main-project/src/api/ability.js
+++ apps/main-project/src/api/ability.js
@@ -1,6 +1,6 @@
 import request from '../utils/request.js';
 
-// 获取专业负责人能力字典
+// 获取专业负责人能力字典\
 export const getProfessionAbility = () => {
   // return request.course.get('/coursemangt/classroommangt/student/list?obsid=' + obsid);
   return request.evaluation.get('/evaluation/ability');
